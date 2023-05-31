32191cff0735bb2778e93d7189fe0f7873359761
Sufyang737 <107362900+Sufyang737@users.noreply.github.com>
2023-05-31 00:10:37 -0300

    subido de archivos
    se adjunta todos los ejercicios con los respectivos códigos e imágenes del circuito.

---
+++ guia-ejer-1/1-d/guia_arduino1.ino
@@ -0,0 +1,36 @@
+#define led1 3
+#define led2 4
+#define led3 2
+#define led4 8
+#define led5 9
+void setup(){
+  pinMode( led1, OUTPUT );
+  pinMode( led2, OUTPUT );
+  pinMode( led3, OUTPUT );
+  pinMode( led4, OUTPUT );
+  pinMode( led5, OUTPUT ); 
+}
+
+void loop()
+{
+  digitalWrite(led1, HIGH);
+  digitalWrite(led4, HIGH);
+  delay(1000); 
+  digitalWrite(led1, LOW);
+  digitalWrite(led4, LOW);
+  delay(1000); 
+  
+  
+  digitalWrite(led2, HIGH);
+  delay(1000); 
+  digitalWrite(led2, LOW);
+  delay(1000); 
+  digitalWrite(led3, HIGH);
+  digitalWrite(led5, HIGH);
+  delay(1000); 
+  digitalWrite(led3, LOW);
+  digitalWrite(led5, LOW);
+  delay(1000); 
+
+
+}
